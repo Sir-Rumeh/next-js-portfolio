ec9ad5d4ef3485d107fc52a53d83f8862edc9a67
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-11-08 13:19:10 +0100

    pushing initial

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+};
+
+module.exports = nextConfig;
