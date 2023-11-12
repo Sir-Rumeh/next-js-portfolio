311b699c03676d5b021951c6dd1dbb40099e6cdf
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-11-12 10:15:35 +0100

    implement animate on scroll for hero section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,42 +9,48 @@ const Main = () => {
 		<div id="home" className="w-full h-screen text-center">
 			<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
 				<div>
-					<h1 className="py-4 text-gray-700">
-						Hi, I&#39;m <span className="text-primary"> Nosarumen</span>
-					</h1>
-					<h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
-					<p className="py-4 text-gray-600 sm:max-w-[70%] m-auto text-lg">
-						I’m focused on building responsive front-end web applications integrating back-end
-						technologies.
-					</p>
-					<p className="py-4 text-gray-600 sm:max-w-[70%] m-auto text-lg">
-						I also do a little bit of backend using node.js
-					</p>
-					<div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-						<a
-							href="https://www.linkedin.com/in/nosarumen-imuentinyan/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in  hover:bg-secondary duration-300">
-								<FaLinkedinIn title="Linkedin" />
-							</div>
-						</a>
-						<a href="https://github.com/Sir-Rumeh/" target="_blank" rel="noreferrer">
-							<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
-								<FaGithub title="GitHub" />
-							</div>
-						</a>
-						<Link href="/#contact">
-							<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
-								<AiOutlineMail title="Contact" />
-							</div>
-						</Link>
-						<Link href="/resume">
-							<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
-								<BsFillPersonLinesFill title="Resume" />
-							</div>
-						</Link>
+					<div data-aos="fade-left">
+						<h1 className="py-4 text-gray-700">
+							Hi, I&#39;m <span className="text-primary"> Nosarumen</span>
+						</h1>
+						<h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
+						<p className="py-4 text-gray-600 sm:max-w-[70%] m-auto text-lg">
+							I’m focused on building responsive front-end web applications integrating back-end
+							technologies.
+						</p>
+						<p className="py-4 text-gray-600 sm:max-w-[70%] m-auto text-lg">
+							I also do a little bit of backend using node.js
+						</p>
+					</div>
+
+					{/*  */}
+					<div data-aos="fade-right">
+						<div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+							<a
+								href="https://www.linkedin.com/in/nosarumen-imuentinyan/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in  hover:bg-secondary duration-300">
+									<FaLinkedinIn title="Linkedin" />
+								</div>
+							</a>
+							<a href="https://github.com/Sir-Rumeh/" target="_blank" rel="noreferrer">
+								<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
+									<FaGithub title="GitHub" />
+								</div>
+							</a>
+							<Link href="/#contact">
+								<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
+									<AiOutlineMail title="Contact" />
+								</div>
+							</Link>
+							<Link href="/resume">
+								<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in hover:bg-secondary duration-300">
+									<BsFillPersonLinesFill title="Resume" />
+								</div>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Navbar from "src/components/Navbar";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		AOS.init({
+			startEvent: "DOMContentLoaded",
+			initClassName: "aos-init",
+			animatedClassName: "aos-animate",
+		});
+	}, []);
 	return (
 		<>
 			<Navbar />
