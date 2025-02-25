cb09dc2d689c4e17613535cec245f94d1fde27ba
Nosarumen Imuentinyan <nosarumenimuentinyan@gmail.com>
2025-02-25 11:27:48 +0100

    Update ProjectItem.tsx

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -12,7 +12,9 @@ interface ItemProps {
 const ProjectItem = ({ title, backgroundImg, tech, projectUrl }: ItemProps) => {
 	return (
 		<div>
-			<div className="relative flex items-center justify-center h-auto w-full md:scale-90 shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-primary to-lightPrimary">
+			<div className="relative flex items-center justify-center h-auto w-full md:scale-90 shadow-xl shadow-gray-400 rounded-xl group 
+  bg-transparent transition-all duration-500 ease-in-out 
+  hover:bg-gradient-to-r from-primary to-lightPrimary">
 				<Image className="rounded-xl group-hover:opacity-10" src={backgroundImg} alt="/" loading="lazy" />
 				<div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
 					<h3 className="text-2xl text-white tracking-wider text-center">{title}</h3>
